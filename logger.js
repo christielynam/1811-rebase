a0724c8b5d9afe3448de017856f95a6eb78948d8
David Whitaker <damwhit@gmail.com>
2019-04-03 11:00:15 -0600

    Plan out lunch

--- logger.js
+++ logger.js
@@ -2,3 +2,6 @@ console.log('march madness!')
 console.log('war eagle!!')
 console.log('beat virginia!')
 console.log('final four!!')
+console.log('I want some pizza. Mebs a burrito too.');
+console.log('SHould I eat a salad instead?');
+console.log('probs not');
